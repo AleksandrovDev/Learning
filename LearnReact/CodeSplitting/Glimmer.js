d4ab686d477c1a3d984a1342509dc5669de741ad
nikita.aleksandrov <nikita.alexandrov@intapp.com>
2023-03-10 11:37:37 +0100

    Added LearnReact

---
+++ LearnReact/CodeSplitting/Glimmer.js
@@ -0,0 +1,3 @@
+export const Glimmer = (props) => {
+  return "Glimmer MODULE"
+}
---
+++ LearnReact/CodeSplitting/myScript.jsx
@@ -0,0 +1,7 @@
+import { Glimmer } from './Glimmer.js'
+const Glimmer2 = (props) => {
+  return <div>Glimmer</div>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(Glimmer));
