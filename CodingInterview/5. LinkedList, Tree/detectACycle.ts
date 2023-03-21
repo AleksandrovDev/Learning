122958179d2e9d002e3390ff11a6986c86c871a7
nikita.aleksandrov <nikita.alexandrov@intapp.com>
2023-03-21 18:46:55 +0100

    Implemented Floyd's cycle detection algorithm

--- CodingInterview/5. LinkedList, Tree/detectACycle.ts
+++ CodingInterview/5. LinkedList, Tree/detectACycle.ts
@@ -3,6 +3,22 @@ interface ListNode {
   next: ListNode | null;
 }
 
+function hasCycleFloydAlgorithm(head: ListNode | null): boolean {
+  let slow = head;
+  let fast = head;
+  
+  while (slow && slow.next && fast?.next?.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    
+    if (slow === fast) {
+      return true; // intersection of slow and fast step => cycle detected
+    }
+  }
+  
+  return false;
+}
+
 function hasCycle(head: ListNode | null): boolean {
   const visitedNodes = new Set();
 
