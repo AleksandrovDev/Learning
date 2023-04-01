cf155115cb5f3cdcb5ad2bfeb70cdcddddde3bf1
nikita.aleksandrov <nikita.alexandrov@intapp.com>
2023-04-01 09:23:18 +0200

    Added CRUD operations

--- Angular 14/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Angular 14/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -24,4 +24,21 @@ export class HeroesComponent {
   ngOnInit(): void {
     this.getHeroes();
   }
+
+  add(heroName: string): void {
+    const name = heroName.trim();
+
+    if (!name) {
+      return;
+    }
+
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h) => h.id !== hero.id);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
 }
