6b34eab742cd9aee9b2c51eb4281ee8bcda14204
Nikita Aleksandrov <mathymath24@gmail.com>
2023-05-01 20:32:18 +0200

    Added routing

--- Angular 14/BudgetTracker/Web/src/app/app-routing.module.ts
+++ Angular 14/BudgetTracker/Web/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MonthViewComponent } from './month-view/month-view.component';
+import { TrackerComponent } from './tracker/tracker.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'month-view',
+    component: MonthViewComponent,
+  },
+  {
+    path: 'tracker',
+    component: TrackerComponent,
+  },
+  {
+    path: '', redirectTo: 'tracker', pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
