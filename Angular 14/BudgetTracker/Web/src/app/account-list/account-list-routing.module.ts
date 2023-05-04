d7049ddc78a10b06d549165061d04c3552c06027
Nikita Aleksandrov <mathymath24@gmail.com>
2023-05-04 11:44:45 +0200

    Added guards for the child routes

--- Angular 14/BudgetTracker/Web/src/app/account-list/account-list-routing.module.ts
+++ Angular 14/BudgetTracker/Web/src/app/account-list/account-list-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountAddComponent } from './account-add/account-add.component';
 import { AccountInfoComponent } from './account-info/account-info.component';
 import { TrackerComponent } from './tracker/tracker.component';
+import { AccountGuard } from './guards/account.guard';
 
 const routes: Routes = [
   {
     // don't forget to clear path if you make this module lazy
     path: '',
     component: TrackerComponent,
+    canActivateChild: [AccountGuard],
     children: [
       {
         path: 'add',
--- Angular 14/BudgetTracker/Web/src/app/login/login.service.ts
+++ Angular 14/BudgetTracker/Web/src/app/login/login.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
   isLoggedIn = false;
+  isAdmin = false;
 
-  constructor() { }
+  constructor() {}
 
   login(email: string, password: string) {
     if (email === 'admin@gmail.com' && password === 'admin') {
       this.isLoggedIn = true;
+      this.isAdmin = true;
       // this.router.navigate(['/accounts', 'add']);
       // this.router.navigateByUrl('/accounts/add');
+    } else if (email === 'user@gmail.com' && password === 'user') {
+      this.isLoggedIn = true;
     }
 
     return this.isLoggedIn;
